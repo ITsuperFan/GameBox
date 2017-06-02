7945e4d2fdcb4c9fa5f08eb6c734f475877404c5
Alan <mr.alan.china.gmail.com>
2017-06-02 22:02:59 +0800

    添加动态及创建3DDemo

---
+++ GameBox/Assets/GBDemo/3D/Create3D.cs
@@ -0,0 +1,121 @@
+﻿/*
+* Copyright (c) 2008-2017 Mr-Alan, Inc.
+* Mail: Mr.Alan.China@gmail.com 
+* Mail: Mr.Alan.China@outlook.com
+* Website: www.0x69h.com
+*/
+
+using UnityEngine;
+
+
+
+namespace Alan
+{
+    public class Create3D : MonoBehaviour 
+	{
+
+        public float h1;
+        public float H;
+        public float R1;
+        public float B;
+        public float R2;
+
+        public Vector3 C;
+        public Vector3 O1;
+
+
+
+
+
+
+
+        public Vector3[] m_VertexArray;
+
+        public int[] m_TriangleArray;
+
+        public Vector2[] m_UV;
+
+        public Texture m_Texture;
+
+        private MeshFilter m_MeshFilter;
+        private MeshRenderer m_MeshRendere;
+        private Mesh m_Mesh;
+        private void Awake()
+        {
+            m_MeshFilter = gameObject.AddComponent<MeshFilter>();
+            m_MeshRendere = gameObject.AddComponent<MeshRenderer>();
+
+
+
+           var Vecs =  PipeRenderHelper.ArcVertex3(Vector2.zero,150f,new Vector2(-150f, 0),new Vector2(150f, 0),100);
+
+            for (int i = 0; i < Vecs.Length; i++)
+            {
+                GameObject go = GameObject.CreatePrimitive( PrimitiveType.Cube );
+                go.name = i.ToString();
+                go.transform.position = new Vector3(Vecs[i].x,Vecs[i].y,0);
+            }
+
+            var Vecs1 = PipeRenderHelper.ArcVertex3(Vector2.zero, 100f, new Vector2(-100f, 0), new Vector2(100f, 0), 100);
+
+            for (int i = 0; i < Vecs1.Length; i++)
+            {
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                go.name = i.ToString();
+                go.transform.position = new Vector3(Vecs1[i].x, Vecs1[i].y, 0);
+            }
+
+
+            m_Mesh = m_MeshFilter.mesh;
+            m_Mesh.vertices = GameBoxFramework.Utility.ArrayHelper.MergeArray<Vector3>(Vecs,0,Vecs.Length,Vecs1,0,Vecs1.Length);
+            m_UV = new Vector2[m_Mesh.vertexCount];
+            m_Mesh.uv = m_UV;
+            m_TriangleArray = new int[(Vecs.Length+Vecs1.Length - 2)*3];
+
+            int t_Index=0;
+            
+            for (int i = 0; i < m_Mesh.vertexCount; i++)
+            {
+                if (i < (m_Mesh.vertexCount / 2 - 1)) //如果在第一层
+                {
+                    m_TriangleArray[t_Index++] = i;
+                    m_TriangleArray[t_Index++] = m_Mesh.vertexCount / 2 + i+1;
+                    m_TriangleArray[t_Index++] = m_Mesh.vertexCount / 2 + i;
+                }
+                else if (i > m_Mesh.vertexCount / 2) //如果在第二层
+                {
+                    m_TriangleArray[t_Index++] = i;
+                    m_TriangleArray[t_Index++] = i-m_Mesh.vertexCount / 2 -1 ;
+                    m_TriangleArray[t_Index++] = i-m_Mesh.vertexCount / 2;
+                }
+            }
+
+        }
+
+        private void Update()
+        {
+            m_Mesh.triangles = m_TriangleArray;
+            m_Mesh.RecalculateNormals();
+
+
+            Material t_Material = new Material(Shader.Find(@"Diffuse"));
+
+            t_Material.mainTexture = m_Texture;
+
+            m_MeshRendere.materials = new Material[] { t_Material };
+
+
+
+            //------------------计算模块
+
+
+            R2 = (Mathf.Pow(B / 2, 2) / 2 + Mathf.Pow(h1, 2) / 2 - (B / 2) * R1)  / (h1 - R1);
+
+
+            //------------------计算模块
+
+
+        }
+
+    }
+}
---
+++ GameBox/Assets/GBDemo/3D/Helper/PipeRenderHelper.cs
@@ -0,0 +1,99 @@
+﻿/*
+* Copyright (c) 2008-2017 Mr-Alan, Inc.
+* Mail: Mr.Alan.China@gmail.com 
+* Mail: Mr.Alan.China@outlook.com
+* Website: www.0x69h.com
+*/
+
+using UnityEngine;
+
+namespace Alan
+{
+    public class PipeRenderHelper 
+	{
+        /// <summary>
+        /// 获取圆弧的顶点数
+        /// </summary>
+        /// <param name="R">圆的半径</param>
+        /// <param name="A">弧形顺时针的第一个开始的点</param>
+        /// <param name="B">弧形顺时针的第二个结束的点</param>
+        /// <returns>顶点数组</returns>
+        public static Vector2[] ArcVertex2(Vector2 o,float r,Vector2 s,Vector2 e,int vertexNumber)
+        {
+            if (vertexNumber <= 2)
+            {
+                throw new System.Exception("顶点数至少为3");
+            }
+            float t_SEDistance = Mathf.Abs(s.x-e.x);
+            float t_MeanDistance = t_SEDistance / (vertexNumber - 1);
+            Vector2[] t_Vertex2 = new Vector2[vertexNumber];
+            t_Vertex2[0] = s;
+            for (int i = 1; i < vertexNumber-1; i++)
+            {
+                t_Vertex2[i] = CircleArcPoint(o,r,s.x+t_MeanDistance*i);
+
+            }
+            t_Vertex2[vertexNumber - 1] = e;
+            return t_Vertex2;
+        }
+
+
+        /// <summary>
+        /// 获取圆弧的顶点数
+        /// </summary>
+        /// <param name="R">圆的半径</param>
+        /// <param name="A">弧形顺时针的第一个开始的点</param>
+        /// <param name="B">弧形顺时针的第二个结束的点</param>
+        /// <returns>顶点数组</returns>
+        public static Vector3[] ArcVertex3(Vector2 o, float r, Vector2 s, Vector2 e, int vertexNumber)
+        {
+            Vector2[] t_Target = ArcVertex2(o,r,s,e,vertexNumber);
+            Vector3[] t_Vertex3 = new Vector3[t_Target.Length];
+            for (int i = 0; i < t_Target.Length; i++)
+            {
+                t_Vertex3[i] = t_Target[i];
+            }
+            return t_Vertex3;
+        }
+
+
+
+
+
+        //public static Vector2[] AveArcVertex(Vector2 o, float r, Vector2 s, Vector2 e, int vertexNumber)
+        //{
+        //    if (vertexNumber <= 2)
+        //    {
+        //        throw new System.Exception("顶点数至少为3");
+        //    }
+        //    float t_Angle = Vector3.Angle(s-o,e-o);
+        //    float t_MeanAngle = t_Angle / (vertexNumber - 1);
+        //    Vector2[] t_Vertex = new Vector2[vertexNumber];
+        //    t_Vertex[0] = s;
+        //    for (int i = 1; i < vertexNumber - 1; i++)
+        //    {
+        //        float x = o.x + (s.x - o.x) * Mathf.Cos(t_MeanAngle) - (s.y - o.y) * Mathf.Sin(t_MeanAngle);
+        //        float y = o.y + (s.x - o.x) * Mathf.Sin(t_MeanAngle) + (s.y - o.y) * Mathf.Cos(t_MeanAngle);
+        //        t_Vertex[i] = new Vector2(x,y);
+        //    }
+        //    t_Vertex[vertexNumber - 1] = s;
+        //    return t_Vertex;
+        //}
+
+
+        /// <summary>
+        /// 通过x轴获取圆弧上的一个正y轴的坐标点
+        /// </summary>
+        /// <param name="o">圆心坐标</param>
+        /// <param name="r">圆的半径</param>
+        /// <param name="x">要求的圆弧上的点的x轴数值</param>
+        /// <returns></returns>
+
+        public static Vector2 CircleArcPoint(Vector2 o,float r,float x)
+        {
+            return new Vector2( x , (Mathf.Sqrt( Mathf.Pow(r, 2) - Mathf.Pow( (x - o.x) , 2 ) ) + o.y)); 
+        }
+
+        
+    }
+}
