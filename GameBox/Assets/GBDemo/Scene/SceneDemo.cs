4f5521b7b898986483b832d430f897d34a2827d3
Alan <mr.alan.china.gmail.com>
2017-06-14 13:34:38 +0800

    添加场景管家并且添加了场景管家测试通过。

---
+++ GameBox/Assets/GBDemo/Scene/SceneDemo.cs
@@ -0,0 +1,68 @@
+﻿/*
+* Copyright (c) 2008-2017 Mr-Alan, Inc.
+* Mail: Mr.Alan.China@gmail.com 
+* Mail: Mr.Alan.China@outlook.com
+* Website: www.0x69h.com
+*/
+
+using GameBox;
+using GameBox.Runtime.Component;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Alan
+{
+	public class SceneDemo : MonoBehaviour 
+	{
+        [SerializeField]
+        private Text m_TextProcess;
+
+        private SceneComponent m_SceneComponent;
+
+        private void Start()
+        {
+
+            m_SceneComponent =  GameBoxEntry.GetComponent<SceneComponent>();
+            m_SceneComponent.Register(this);
+            m_SceneComponent.LoadUpdateEventHandler += (o, e) => { m_TextProcess.text = (e as SceneEventArgs).SceneName + " : " + (e as SceneEventArgs).Process; };
+        }
+
+        [SceneEvent( SceneEventType.LoadSuccess)]
+        private void SceneEventDemo1(object o,SceneEventArgs e)
+        {
+            Debug.Log("All " + e.SceneName+" : "+e.Process);
+        }
+
+        [SceneEvent(SceneEventType.LoadSuccess, "A")]
+        private void SceneEventDemo2(object o, SceneEventArgs e)
+        {
+            Debug.Log("Single " + e.SceneName + " : " + e.Process);
+        }
+
+        private void OnGUI()
+        {
+
+            if (GUILayout.Button("Load A Scene"))
+            {
+                m_SceneComponent.LoadScene("A", LoadSceneModle.Append,true);
+
+            }
+
+            if (GUILayout.Button("Load B Scene"))
+            {
+                m_SceneComponent.LoadScene("B", LoadSceneModle.Append, true);
+
+            }
+
+            if (GUILayout.Button("Load C Scene"))
+            {
+                m_SceneComponent.LoadScene("C", LoadSceneModle.Append, true);
+
+            }
+        }
+
+
+    }
+}
---
+++ GameBox/Assets/GameBox/GameBox/Runtime/Components/Scene/SceneEventArgs.cs
@@ -0,0 +1,36 @@
+﻿/*
+* Copyright (c) 2008-2017 Mr-Alan, Inc.
+* Mail: Mr.Alan.China@gmail.com 
+* Mail: Mr.Alan.China@outlook.com
+* Website: www.0x69h.com
+*/
+
+
+
+using GameBoxFramework;
+using UnityEngine.SceneManagement;
+
+namespace GameBox.Runtime.Component
+{
+    /// <summary>
+    /// 场景事件参数
+    /// </summary>
+    public class SceneEventArgs : BaseEventArgs 
+	{
+        /// <summary>
+        /// 场景
+        /// </summary>
+        public Scene Scene { get; set; }
+
+        /// <summary>
+        /// 场景名
+        /// </summary>
+		public string SceneName { get; set; }
+
+        /// <summary>
+        ///  进度
+        /// </summary>
+		public float Process { get; set; }
+
+    }
+}
